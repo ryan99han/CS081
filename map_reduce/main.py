a10a6f2560fa2b899431a70ff53cdb6a8e4c1a4a
Ryan Han <ryan99han@gmail.com>
2021-01-25 14:14:53 -1000

    WIP: Add mapper functionality

--- map_reduce/main.py
+++ map_reduce/main.py
@@ -1,22 +1,48 @@
 from multiprocessing.pool import ThreadPool
+from filelock import FileLock
+from os import walk
+
 import threading
 import time
 import random
+import string
 
-MAX_THREADS = 10
+MAX_THREADS = 1
 print_lock = threading.Lock()
+EXCLUDE_CHARS = string.punctuation + string.digits
 
-def thread_function(num):
+def add_to_word_file(word):
+    wordfile = word + ".txt"
+    with FileLock(wordfile + ".lock"):
+        with open(word_file, 'a') as wf:
+            wf.write(word + " 1")
+
+def get_words_from_line(line):
+    return line.translate(str.maketrans('', '', EXCLUDE_CHARS)).split(" ")
+
+def mapper(filename):
+    filepath = "text/" + filename
+    with open(filepath) as fp:
+        line = fp.readline()[:-1]
+        while line:
+            line = get_words_from_line(line)
+            for word in line:
+                add_to_word_file(word)
+            break
+            line = fp.readLine()
+    fp.close()
     with print_lock:
-        print("Processed %i", num)
+        print("Processed file: ", filename)
 
 if __name__ == "__main__":
+    _, _, filenames = next(walk("text/"))
+    filenames = filenames[:1] # DEBUG
+    print(filenames)
     pool = ThreadPool(processes=MAX_THREADS)
-    results = []
-    lst = [i for i in range(25)]
-    while lst:
-        num = lst.pop()
-        results.append(pool.apply_async(thread_function, (num, )))
+
+    while filenames:
+        filename = filenames.pop()
+        pool.apply_async(mapper, (filename, ))
 
     pool.close() # Stop adding tasks to pool
     pool.join()  # Wait for all tasks to complete
