56e4e051f9f3c8b5662f452e7a61e5c36778d925
Ryan Han <ryan99han@gmail.com>
2021-02-19 00:25:49 -1000

    Add WIP implementation

---
+++ cs90/ds/main/mrmaster.go
@@ -0,0 +1,28 @@
+package main
+
+//
+// Start the master process, which is implemented in ../mr/master.go
+// go run mrmaster.go pg*.txt
+//
+
+import (
+    "cs90/ds/mr"
+    "time"
+    "os"
+    "fmt"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "Usage: mrmaster inputfiles...\n")
+		os.Exit(1)
+	}
+
+	m := mr.MakeMaster(os.Args[1:], 10)
+	for m.Done() == false {
+        fmt.Printf("inside for loop")
+		time.Sleep(time.Second)
+	}
+
+	time.Sleep(time.Second)
+}
---
+++ cs90/ds/main/mrworker.go
@@ -0,0 +1,48 @@
+package main
+
+//
+// Start a worker process, which is implemented in ../mr/worker.go. 
+// Typically there will be multiple worker processes, talking to one master.
+//
+// go run mrworker.go wc.so
+//
+import (
+    "cs90/ds/mr"
+    "plugin"
+    "os"
+    "fmt"
+    "log"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: mrworker xxx.so\n")
+		os.Exit(1)
+	}
+
+	mapf, reducef := loadPlugin(os.Args[1])
+	mr.Worker(mapf, reducef)
+}
+
+//
+// Load the application Map and Reduce functions from a plugin file, 
+// e.g. ../mrapps/wc.so
+//
+func loadPlugin(filename string) (func(string, string) []mr.KeyValue, func(string, []string) string) {
+	p, err := plugin.Open(filename)
+	if err != nil {
+		log.Fatalf("cannot load plugin %v", filename)
+	}
+	xmapf, err := p.Lookup("Map")
+	if err != nil {
+		log.Fatalf("cannot find Map in %v", filename)
+	}
+	mapf := xmapf.(func(string, string) []mr.KeyValue)
+	xreducef, err := p.Lookup("Reduce")
+	if err != nil {
+		log.Fatalf("cannot find Reduce in %v", filename)
+	}
+	reducef := xreducef.(func(string, []string) string)
+
+	return mapf, reducef
+}
